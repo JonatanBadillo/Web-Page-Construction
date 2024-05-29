e70e09863f2c9957835e9761b0cdee33af65833b
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-29 17:07:21 -0600

    chore: Update Navbar component with theme toggle functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,12 @@ import Navbar from './Components/Navbar/Navbar'
 
 
 const App = () => {
+
+  const [theme, setTheme] = React.useState('light')
+
   return (
     <div className='container'>
-      <Navbar />
+      <Navbar theme={theme} setTheme={setTheme} />
     </div>
   )
 }
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -7,12 +7,16 @@ import search_icon_dark from '../../assets/search-b.png'
 import toogle_light from '../../assets/night.png'
 import toogle_dark from '../../assets/day.png'
 
-const Navbar = () => {
+const Navbar = ({theme,setTheme}) => {
+    const toogle_mode = () => {
+        theme === 'light' ? setTheme('dark') : setTheme('light');
+    }
     return (
         <div>
                 <div className='navbar'>
                         {/* TODO: CHANGE IMG TO THE LOGO */}
-                        <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png' alt='Logo' className='logo'/>
+                        {/*<img src={theme == 'light' ?  logo_light : logo_dark } alt='Logo' className='logo'/>*/}
+                        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="logo" className='logo' />
                         <ul>
                             <li>Home</li>
                             <li>About Us</li>
@@ -23,10 +27,10 @@ const Navbar = () => {
                 
                 <div className='search-box'>
                     <input type='text' placeholder='Search' />
-                    <img src={search_icon_light} alt="" />
+                    <img src={theme == 'light' ? search_icon_light : search_icon_dark} alt="" />
                 </div>
 
-                <img src={toogle_light} alt="" className='toogle-icon'/>
+                <img onClick={()=>{toogle_mode()}} src={toogle_light} alt="" className='toogle-icon'/>
                 
                 </div>
         </div>
