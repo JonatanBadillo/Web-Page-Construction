799c17e9c805540b9feb7e379e2c35dac3e3df16
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-29 14:13:07 -0600

    chore: Add Navbar component to App

---
+++ src/Components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div>
+        <div className='navbar'>
+            <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png' alt='Google Logo' />
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
