b9b6825551518ff5bebce912fd678595b04aa0f3
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-29 14:20:38 -0600

    chore: Update Navbar component with logo and search functionality

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,13 +2,27 @@ import React from 'react'
 import './Navbar.css'
 
 const Navbar = () => {
-  return (
-    <div>
-        <div className='navbar'>
-            <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png' alt='Google Logo' />
+    return (
+        <div>
+                <div className='navbar'>
+                        {/* TODO: CHANGE IMG TO THE LOGO */}
+                        <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png' alt='Logo' />
+                        <ul>
+                            <li>Home</li>
+                            <li>About Us</li>
+                            <li>See Our Work</li>
+                            <li>Contact Us</li>
+                        </ul>
+                </div>
+                
+                <div className='search-box'>
+                    <input type='text' placeholder='Search' />
+                    <img src="" alt="" />
+                </div>
+
+                <img src="" alt="" className='toggle-icon'/>
         </div>
-    </div>
-  )
+    )
 }
 
 export default Navbar
