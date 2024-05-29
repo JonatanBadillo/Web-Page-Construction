077527ff766373f3a1f74fe93a449c424ce15e76
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-05-29 14:41:12 -0600

    chore: Update Navbar component with logo, search functionality, and styling

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -12,21 +12,23 @@ const Navbar = () => {
         <div>
                 <div className='navbar'>
                         {/* TODO: CHANGE IMG TO THE LOGO */}
-                        <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png' alt='Logo' />
+                        <img src='https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png' alt='Logo' className='logo'/>
                         <ul>
                             <li>Home</li>
                             <li>About Us</li>
                             <li>See Our Work</li>
                             <li>Contact Us</li>
                         </ul>
-                </div>
+                
                 
                 <div className='search-box'>
                     <input type='text' placeholder='Search' />
                     <img src={search_icon_light} alt="" />
                 </div>
 
-                <img src={toogle_light} alt="" className='toggle-icon'/>
+                <img src={toogle_light} alt="" className='toogle-icon'/>
+                
+                </div>
         </div>
     )
 }
