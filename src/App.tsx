026c56b3ec865f91a70fd5637d92cd71798c4022
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-01 13:34:08 -0600

    chore: Update package dependencies for react-icons and react-image-gallery

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
 import { useEffect, useState } from 'react'
 import Navbar from './Components/Navbar/Navbar'
 import Cards from './Components/Cards/Cards';
+import ImageGallery from 'react-image-gallery';
+import 'react-image-gallery/styles/css/image-gallery.css';
 
 
 
 const App = () => {
+
+  const images = [
+    {
+      original : 'https://picsum.photos/id/1018/1000/600/',
+      thumbnail : 'https://picsum.photos/id/1018/250/150/', 
+    },
+    {
+      original : 'https://picsum.photos/id/1015/1000/600/',
+      thumbnail : 'https://picsum.photos/id/1015/250/150/', 
+    },
+    {
+      original : 'https://picsum.photos/id/1019/1000/600/',
+      thumbnail : 'https://picsum.photos/id/1019/250/150/', 
+    },
+    {
+      original : 'https://picsum.photos/id/1016/1000/600/',
+      thumbnail : 'https://picsum.photos/id/1016/250/150/',
+    }
+  ]
 
   const currentTheme = localStorage.getItem('currentTheme');
 
@@ -18,6 +39,7 @@ const App = () => {
     <div className={`container ${theme}`}>
       <Navbar theme={theme} setTheme={setTheme} />
       <Cards />
+      <ImageGallery items={images} />
     </div>
   )
 }
