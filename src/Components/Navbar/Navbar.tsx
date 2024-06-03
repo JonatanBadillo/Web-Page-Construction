4a63aed8fb6102a5e92ea9bdd52ce431eeb00ff5
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-03 14:32:36 -0600

    chore: Update package dependencies to include @mui/icons-material 5.15.19

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -1,39 +1,109 @@
-import './Navbar.css'
-// import logo_light from ''
-// import logo_dark from ''
-import search_icon_light from '../../assets/search-w.png'
-import search_icon_dark from '../../assets/search-b.png'
-import toogle_light from '../../assets/night.png'
-import toogle_dark from '../../assets/day.png'
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import Drawer from '@mui/material/Drawer';
+import IconButton from '@mui/material/IconButton';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
+import MenuIcon from '@mui/icons-material/Menu';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
 
-const Navbar = ({theme,setTheme}) => {
-    const toogle_mode = () => {
-        theme === 'light' ? setTheme('dark') : setTheme('light');
-    }
-    return (
-        <div>
-                <div className='navbar'>
-                        {/* TODO: CHANGE IMG TO THE LOGO */}
-                        {/*<img src={theme == 'light' ?  logo_light : logo_dark } alt='Logo' className='logo'/>*/}
-                        <img src="https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_74x24dp.png" alt="logo" className='logo' />
-                        <ul>
-                            <li>Home</li>
-                            <li>About Us</li>
-                            <li>See Our Work</li>
-                            <li>Contact Us</li>
-                        </ul>
-                
-                
-                <div className='search-box'>
-                    <input type='text' placeholder='Search' />
-                    <img src={theme == 'light' ? search_icon_light : search_icon_dark} alt="" />
-                </div>
-
-                <img onClick={()=>{toogle_mode()}} src={theme == 'light' ? toogle_light : toogle_dark} alt="" className='toogle-icon'/>
-                
-                </div>
-        </div>
-    )
+interface Props {
+    /**
+     * Injected by the documentation to work in an iframe.
+     * You won't need it on your project.
+     */
+    window?: () => Window;
 }
 
-export default Navbar
+const drawerWidth = 240;
+const navItems = ['Home', 'About Us','Our Work' ,'Contact Us'];
+
+export default function DrawerAppBar(props: Props) {
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+      Sam Alexander Construction Inc.
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: 'center' }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container = window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: 'flex' }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1, display: { xs: 'none', sm: 'block' } }}
+          >
+            Sam Alexander Construction Inc.
+          </Typography>
+          <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
+            {navItems.map((item) => (
+              <Button key={item} sx={{ color: '#fff' }}>
+                {item}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: 'block', sm: 'none' },
+            '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        
+      </Box>
+    </Box>
+  );
+}
