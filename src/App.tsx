b1aa2bcffbb85084a8cba517b38f66cc4004b573
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-08 15:33:03 -0600

    chore: Add banner with overlay and text to landing page

--- src/App.tsx
+++ src/App.tsx
@@ -36,6 +36,34 @@ const App = () => {
       
       <Navbar/>
 
+
+<div style={{ position: 'relative', textAlign: 'center', color: 'white', marginTop: '0px' }}>
+  {/* Agregar un pseudo-elemento ::before para oscurecer la imagen de fondo */}
+  <div style={{
+    position: 'absolute',
+    top: '0',
+    left: '0',
+    width: '100%',
+    height: '100%',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)', // la opacidad 
+    zIndex: '1',
+  }}></div>
+  <img src="src/assets/banner.webp" alt="Banner" style={{ width: '100%', height: 'auto' }} />
+  <div style={{
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    zIndex: '2', // texto y el botón estén por encima del overlay oscuro
+    padding: '20px',
+    borderRadius: '10px'
+  }}>
+    <h1 style={{ textShadow: '2px 2px 4px #000000' }}>Building Dreams Together</h1>
+    <p style={{ textShadow: '2px 2px 4px #000000' }}>Creating our ideal space with top-notch construction.</p>
+    <button style={{ padding: '10px 20px', fontSize: '16px', backgroundColor: '#ffdb58', color: 'black', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>Request consultation</button>
+  </div>
+</div>
+
       <div className="card-container">
         <CustomCard
           imageSrc="src/assets/company3.jpg"
