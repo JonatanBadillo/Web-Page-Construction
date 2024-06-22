0ebb4b5cf246fd33c382334d18e053479fbad57e
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-22 17:40:42 -0600

    chore: Update Form component styling and responsiveness

--- src/Components/Form/Form.tsx
+++ src/Components/Form/Form.tsx
@@ -1,38 +1,44 @@
-import './Form.css'
+import './Form.css';
 
 const Form = () => {
   return (
     <div>
-        <form className="form-container">
-        <label>Name:</label>
-        <input type="text" name="name" />
-
-        <label>Last Name:</label>
-        <input type="text" name="lastName" />
-
-        <label>Email:</label>
-        <input type="email" name="email" />
-
-
-
-        <label>Phone number:</label>
-        <input type="tel" name="phone" />
-
-        <label>Reason for contact:</label>
-        <select name="reason">
-        <option value="inquiry">Project Inquiry</option>
-          <option value="doubt">Doubt</option>
-          <option value="complain">Complain</option>
-          <option value="suggestion">Suggestion</option>
-
-        </select>
-
-
-
-      <button type="submit">Submit</button>
-    </form>
+      <form className="form-container">
+        <div className="form-row">
+          <div className="form-col">
+            <label>Name:</label>
+            <input type="text" name="name" />
+          </div>
+          <div className="form-col">
+            <label>Last Name:</label>
+            <input type="text" name="lastName" />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-col">
+            <label>Email:</label>
+            <input type="email" name="email" />
+          </div>
+          <div className="form-col">
+            <label>Phone number:</label>
+            <input type="tel" name="phone" />
+          </div>
+        </div>
+        <div className="form-row">
+          <div className="form-col">
+            <label>Reason for contact:</label>
+            <select name="reason">
+              <option value="inquiry">Project Inquiry</option>
+              <option value="doubt">Doubt</option>
+              <option value="complain">Complain</option>
+              <option value="suggestion">Suggestion</option>
+            </select>
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Form
+export default Form;
