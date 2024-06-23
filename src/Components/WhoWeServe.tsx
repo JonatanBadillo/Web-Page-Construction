ec31e11b946f10abd7eb6cb78a1a26473dff32e9
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-22 18:35:35 -0600

    chore: Update WhoWeServe component with new icons

--- src/Components/WhoWeServe.tsx
+++ src/Components/WhoWeServe.tsx
@@ -1,56 +1,45 @@
-import './WhoWeServe.css';
-//import multifamilyIcon from '../../assets/multifamily.png';  
-//import commercialIcon from '../../assets/commercial.png';
-//import architectsIcon from '../../assets/architects.png';
+import "./WhoWeServe.css";
+
 import { FaBuilding } from "react-icons/fa";
 import { BsFillBuildingsFill } from "react-icons/bs";
 import { HiBuildingOffice2 } from "react-icons/hi2";
 import { TbBuildingEstate } from "react-icons/tb";
 import { RiBuilding2Line } from "react-icons/ri";
 
-
-
-
-
 const WhoWeServe = () => {
   return (
     <div className="who-we-serve">
       <h2>WHO WE SERVE</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia sunt unde, nam odit, necessitatibus error labore quo fugit consequuntur modi tempore minima. Dolores sequi quisquam ut quaerat, beatae minima vel!
-          </p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia sunt unde,
+        nam odit, necessitatibus error labore quo fugit consequuntur modi
+        tempore minima. Dolores sequi quisquam ut quaerat, beatae minima vel!
+      </p>
       <div className="service-icons">
         <div className="service-icon">
-        <FaBuilding />
-          {/* <img src={multifamilyIcon} alt="Multi Family" /> */}
-          
+          <FaBuilding />
 
           <h3>MULTI FAMILY</h3>
         </div>
         <div className="service-icon">
-        <BsFillBuildingsFill />
-          {/* <img src={commercialIcon} alt="Commercial" /> */}
+          <BsFillBuildingsFill />
+
           <h3>COMMERCIAL</h3>
         </div>
         <div className="service-icon">
-        <HiBuildingOffice2 />
-          {/* <img src={architectsIcon} alt="Architects + Engineers" /> */}
+          <HiBuildingOffice2 />
+
           <h3>PROPERTY MANAGMENT</h3>
         </div>
 
         <div className="service-icon">
-        <TbBuildingEstate />
-
-          {/* <img src={multifamilyIcon} alt="Multi Family" /> */}
-          
+          <TbBuildingEstate />
 
           <h3>HOSPITALITY</h3>
         </div>
 
         <div className="service-icon">
-        <RiBuilding2Line />
-          {/* <img src={multifamilyIcon} alt="Multi Family" /> */}
-          
+          <RiBuilding2Line />
 
           <h3>NON PROFIT</h3>
         </div>
