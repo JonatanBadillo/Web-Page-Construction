75c3360a60c1346f5756502ace657bfbf85a381d
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-06-22 18:42:43 -0600

    chore: Update WhoWeServe component with new icons

--- src/Components/WhoWeServe.tsx
+++ src/Components/WhoWeServe.tsx
@@ -1,5 +1,5 @@
-import "./WhoWeServe.css";
 
+import './WhoWeServe.css';
 import { FaBuilding } from "react-icons/fa";
 import { BsFillBuildingsFill } from "react-icons/bs";
 import { HiBuildingOffice2 } from "react-icons/hi2";
@@ -11,36 +11,27 @@ const WhoWeServe = () => {
     <div className="who-we-serve">
       <h2>WHO WE SERVE</h2>
       <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia sunt unde,
-        nam odit, necessitatibus error labore quo fugit consequuntur modi
-        tempore minima. Dolores sequi quisquam ut quaerat, beatae minima vel!
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quia sunt unde, nam odit, necessitatibus error labore quo fugit consequuntur modi tempore minima. Dolores sequi quisquam ut quaerat, beatae minima vel!
       </p>
       <div className="service-icons">
         <div className="service-icon">
           <FaBuilding />
-
           <h3>MULTI FAMILY</h3>
         </div>
         <div className="service-icon">
           <BsFillBuildingsFill />
-
           <h3>COMMERCIAL</h3>
         </div>
         <div className="service-icon">
           <HiBuildingOffice2 />
-
-          <h3>PROPERTY MANAGMENT</h3>
+          <h3>PROPERTY MANAGEMENT</h3>
         </div>
-
         <div className="service-icon">
           <TbBuildingEstate />
-
           <h3>HOSPITALITY</h3>
         </div>
-
         <div className="service-icon">
           <RiBuilding2Line />
-
           <h3>NON PROFIT</h3>
         </div>
       </div>
