48ae19bf364f01be29109404d6349f6bf12237ff
Jonatan Badillo <jonatanbadillo.19@gmail.com>
2024-12-11 13:50:51 -0600

    Refactor icon imports in WhoWeServe.tsx: Replace existing icons with Material Design icons for improved visual consistency and clarity in service representation.

--- src/Components/WhoWeServe.tsx
+++ src/Components/WhoWeServe.tsx
@@ -1,39 +1,40 @@
-
 import './WhoWeServe.css';
-import { FaBuilding } from "react-icons/fa";
-import { BsFillBuildingsFill } from "react-icons/bs";
-import { HiBuildingOffice2 } from "react-icons/hi2";
-import { TbBuildingEstate } from "react-icons/tb";
-import { RiBuilding2Line } from "react-icons/ri";
+import { 
+  MdApartment, // Para Multi Family
+  MdStorefront, // Para Commercial
+  MdBusinessCenter, // Para Property Management
+  MdHotel, // Para Hospitality
+  MdHandshake // Para Non Profit
+} from "react-icons/md";
 
 const WhoWeServe = () => {
   return (
     <div className="who-we-serve">
       <h2>Who We Serve</h2>
       <p>
-      At Sam Alexander Construction Inc., we view every partnership as an opportunity to help
-builders and developers achieve their vision. Our services are designed to
-integrate seamlessly into your workflow, ensuring a hassle-free and productive collaboration.
+        At Sam Alexander Construction Inc., we view every partnership as an opportunity to help
+        builders and developers achieve their vision. Our services are designed to
+        integrate seamlessly into your workflow, ensuring a hassle-free and productive collaboration.
       </p>
       <div className="service-icons">
         <div className="service-icon">
-          <FaBuilding />
+          <MdApartment />
           <h3>MULTI FAMILY</h3>
         </div>
         <div className="service-icon">
-          <BsFillBuildingsFill />
+          <MdStorefront />
           <h3>COMMERCIAL</h3>
         </div>
         <div className="service-icon">
-          <HiBuildingOffice2 />
+          <MdBusinessCenter />
           <h3>PROPERTY MANAGEMENT</h3>
         </div>
         <div className="service-icon">
-          <TbBuildingEstate />
+          <MdHotel />
           <h3>HOSPITALITY</h3>
         </div>
         <div className="service-icon">
-          <RiBuilding2Line />
+          <MdHandshake />
           <h3>NON PROFIT</h3>
         </div>
       </div>
